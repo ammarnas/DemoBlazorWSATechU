e7843efc312a0f22a085108fd1b2a971031956fd
ammarnas <ammarnasser.contact@gmail.com>
2026-04-04 00:20:04 +0300

    25. Blazor  ｜ انشاء قاعدة البيانات والجداول

---
+++ DemoEmployee.Client/DemoEmployee.Core/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace DemoEmployee.Core
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Department { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime DateOfJoining { get; set; }
+
+    }
+}
---
+++ DemoEmployee.Client/DemoEmployee.Server/Data/DBContext.cs
@@ -0,0 +1,13 @@
+﻿using DemoEmployee.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoEmployee.Server.Data
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employee{ get; set; }
+    }
+}
