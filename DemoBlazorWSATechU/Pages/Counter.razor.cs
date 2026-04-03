2351c09d605d05054ac74ed61e7125369b940484
ammarnas <ammarnasser.contact@gmail.com>
2026-04-03 21:31:31 +0300

    19. Blazor  ｜ Binding to Select

--- DemoBlazorWSATechU/Pages/Counter.razor.cs
+++ DemoBlazorWSATechU/Pages/Counter.razor.cs
@@ -5,14 +5,32 @@ namespace DemoBlazorWSATechU.Pages
     public partial class Counter
     {
         public AddClass AddClass { get; set; }
+        public Char Operator { get; set; }
 
         public Counter()
         {
             AddClass = new AddClass();
+            Operator = '+';
         }
-        public void Add()
+        public void Process()
         {
-            AddClass.Result = AddClass.FirstNumber + AddClass.SecondNumber;
+            switch (Operator)
+            {
+                case '+':
+                    AddClass.Result = AddClass.FirstNumber + AddClass.SecondNumber;
+                    break;
+                case '-':
+                    AddClass.Result = AddClass.FirstNumber - AddClass.SecondNumber;
+                    break;
+                case '*':
+                    AddClass.Result = AddClass.FirstNumber * AddClass.SecondNumber;
+                    break;
+                case '/':
+                    AddClass.Result = AddClass.FirstNumber / AddClass.SecondNumber;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
